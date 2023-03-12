c672ae160ecb327d996b5c0807ef5237aaac8eba
Aukown1003 <115121908+Aukown1003@users.noreply.github.com>
2023-03-12 03:10:06 +0000

    Add_room切り替え、フレンドの検索、一覧

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,27 @@
 class Public::UsersController < ApplicationController
   def show
     # current_user以外を取得
-    @users = User.all.where.not(id: current_user.id)
+    # @users = User.all.where.not(id: current_user.id)
+    @users = current_user.following_users
     @rooms = current_user.rooms
+    
+    # 何かしらのユーザーの検索を行った時
+    word = params[:word]
+    unless word.nil? || word.blank?
+      range = params[:range]
+      if range == "Name"
+        @users = User.where.not(id: current_user.id).where("name LIKE?", "%#{word}%")
+      elsif range == "Nickname"
+        @users = User.where.not(id: current_user.id).where("nickname LIKE?", "%#{word}%")
+      else
+        @users = User.where.not(id: current_user.id).where("search_id LIKE?", "%#{word}%")
+      end
+    end
+    
+    # roomの切り替え処理
+    if params[:room_id].present?
+      @room = Room.find(params[:room_id])
+    end
+    
   end
 end
